11e427c010447bf190507cfb8cecf953090addb4
Kooreis <kooreis.official@gmail.com>
2024-11-16 22:20:34 -0600

    In the second update, the acquire method is added to the RateLimiter class. This method calculates the elapsed time since the last token was added, updates the token count, and adjusts the token count based on the max_rate. If there are less than 1 token, the method will pause the thread until a token is available.

--- solution.py
+++ solution.py
@@ -1,9 +1,13 @@
-import time
-import threading
-
-class RateLimiter:
-    def __init__(self, max_rate):
-        self.tokens = 0
-        self.last = time.time()
-        self.lock = threading.Lock()
-        self.max_rate = max_rate
+def acquire(self):
+        with self.lock:
+            now = time.time()
+            elapsed = now - self.last
+            self.last = now
+            self.tokens += elapsed * self.max_rate
+            if self.tokens > self.max_rate:
+                self.tokens = self.max_rate
+            if self.tokens < 1:
+                time.sleep(1 - self.tokens)
+                self.tokens -= 1
+            else:
+                self.tokens -= 1
